352a55a05ca1695e4d95566cde54567c6f9b9143
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-29 10:47:18 -0500

    Add form for new book and push to array

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 let myLibrary = [
-    { name: "To sell is human", author: "tony", pages: 200, read: true },
-    { name: "The slammer", author: "john", pages: 350, read: false }
+  { name: "To sell is human", author: "tony", pages: 200, read: true },
+  { name: "The slammer", author: "john", pages: 350, read: false }
 ];
 
 let cardClone = document.getElementById("card").cloneNode(true);
+const btn = document.getElementById("btn")
 
-function Book() {
-    // the constructor...
+
+
+function Book(name, author, pages, read) {
+  this.name = name,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
 }
+
+// Book.prototype.read = function () {
+
+// }
 
 function render() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        let card = cardClone.cloneNode(true);
-        card.querySelector(".book-title").innerHTML = myLibrary[i].name;
-        card.querySelector(".book-author").innerHTML = myLibrary[i].author;
-        card.querySelector(".book-pages").innerHTML = myLibrary[i].pages;
-        card.querySelector(".book-read").innerHTML = myLibrary[i].read;
-        console.log(card)
-        document.querySelector(".row").appendChild(card);
-    }
+  for (let i = 0; i < myLibrary.length; i++) {
+    let card = cardClone.cloneNode(true);
+    card.querySelector(".book-title").innerHTML = myLibrary[i].name;
+    card.querySelector(".book-author").innerHTML = myLibrary[i].author;
+    card.querySelector(".book-pages").innerHTML = myLibrary[i].pages;
+    card.querySelector(".book-read").innerHTML = myLibrary[i].read;
+    document.querySelector(".row").appendChild(card);
+  }
 }
-
-
 
 function addBookToLibrary() {
-    // do stuff here
+  const form = document.querySelector('#form');
+  const name = form.querySelector("#name").value;
+  const author = form.querySelector("#author").value;
+  const pages = form.querySelector("#pages").value;
+  const read = form.querySelector("#read").value;
+
+  const book = new Book(name, author, pages, read);
+
+  myLibrary.push(book)
+  console.log(myLibrary)
+  render();
 }
 
-render();
+document.addEventListener('onload', render());
 
