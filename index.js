bb6f5c2aa662c042acbb2621c31f64c2182079af
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-31 15:59:44 -0500

    Fix css and js style errors

--- index.js
+++ index.js
@@ -1,103 +1,107 @@
-let myLibrary = [
-];
 
-const book1 = new Book("To sell is human", "tony", 200, true);
-const book2 = new Book("The Park", "john", 350, false);
+const myLibrary = [];
+
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.readStatus = function readStatus() {
+  this.read = !this.read;
+};
+
+const book1 = new Book('To sell is human', 'tony', 200, true);
+const book2 = new Book('The Park', 'john', 350, false);
 
 myLibrary.push(book1);
 myLibrary.push(book2);
 
-const cardClone = document.getElementById("card").cloneNode(true);
-cardClone.classList.remove("hidden");
-const btn = document.getElementById("btn");
-const displayBtn = document.getElementById("display");
+const cardClone = document.getElementById('card').cloneNode(true);
+cardClone.classList.remove('hidden');
 const form = document.querySelector('#form');
 
 function showForm() {
-  const form_wrap = document.querySelector('.form-wrap');
-  form_wrap.classList.toggle("hidden");
-}
-
-function Book(name, author, pages, read) {
-  this.name = name,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
-}
-
-Book.prototype.readStatus = function () {
-  this.read = this.read ? false : true;
-}
-
-function toggleRead(button) {
-  const list = button.classList
-  let bookIndex = list[list.length - 1];
-  myLibrary[bookIndex].readStatus();
-  render();
-}
-
-function remove(book) {
-  const list = book.classList
-  let bookIndex = list[list.length - 1];
-  myLibrary.splice(bookIndex, 1);
-  render();
+  const formWrap = document.querySelector('.form-wrap');
+  formWrap.classList.toggle('hidden');
 }
 
 function createCard(i) {
-  let card = cardClone.cloneNode(true);
-  card.querySelector(".book-title").innerHTML = myLibrary[i].name;
-  card.querySelector(".book-author").innerHTML = myLibrary[i].author;
-  card.querySelector(".book-pages").innerHTML = myLibrary[i].pages;
-  card.querySelector(".book-read").innerHTML = myLibrary[i].read;
-  card.querySelector(".toggleread").classList.add(`${i}`);
-  card.querySelector(".remove").classList.add(`${i}`)
-  return card
+  const card = cardClone.cloneNode(true);
+  card.querySelector('.book-title').innerHTML = myLibrary[i].name;
+  card.querySelector('.book-author').innerHTML = myLibrary[i].author;
+  card.querySelector('.book-pages').innerHTML = myLibrary[i].pages;
+  card.querySelector('.book-read').innerHTML = myLibrary[i].read;
+  card.querySelector('.toggleread').classList.add(`${i}`);
+  card.querySelector('.remove').classList.add(`${i}`);
+  return card;
+}
+
+function clearDOM() {
+  document.querySelector('.row').innerHTML = '';
 }
 
 function render() {
   clearDOM();
-  for (let i = 0; i < myLibrary.length; i++) {
+  for (let i = 0; i < myLibrary.length; i += 1) {
     const card = createCard(i);
-    const readStatus = card.querySelector(".book-read").textContent;
-    let readToggle = card.querySelector(".toggleread");
-    if (readStatus === "false") {
-      readToggle.textContent = "Read"
+    const readStatus = card.querySelector('.book-read').textContent;
+    const readToggle = card.querySelector('.toggleread');
+    if (readStatus === 'false') {
+      readToggle.textContent = 'Read';
     } else {
-      readToggle.textContent = "Unread"
+      readToggle.textContent = 'Unread';
     }
-    document.querySelector(".row").appendChild(card);
+    document.querySelector('.row').appendChild(card);
   }
 }
 
+function toggleRead(button) {
+  const list = button.classList;
+  const bookIndex = list[list.length - 1];
+  myLibrary[bookIndex].readStatus();
+  render();
+}
+
+document.querySelector('.toggleread').addEventListener('click', toggleRead);
+
+function remove(book) {
+  const list = book.classList;
+  const bookIndex = list[list.length - 1];
+  myLibrary.splice(bookIndex, 1);
+  render();
+}
+
+document.querySelector('.remove').addEventListener('click', remove);
+
 function createBook() {
-  const errorMsg = form.querySelector(".error");
-  const name = form.querySelector("#name");
-  const author = form.querySelector("#author");
-  const pages = form.querySelector("#pages");
-  const read = form.querySelector("#read").checked;
+  const errorMsg = form.querySelector('.error');
+  const name = form.querySelector('#name');
+  const author = form.querySelector('#author');
+  const pages = form.querySelector('#pages');
+  const read = form.querySelector('#read').checked;
   const fields = [name, author, pages];
   let valid = true;
   // let result = /^[a-zA-Z ]+$/.test('John Doe');
   // console.log(result);
 
-  for (let i = 0; i < fields.length; i++) {
-    if (fields[i].value === "") {
+  for (let i = 0; i < fields.length; i += 1) {
+    if (fields[i].value === '') {
       valid = false;
-      console.log(fields[i])
-      fields[i].style.borderColor = "red";
+      fields[i].style.borderColor = 'red';
     } else {
-      fields[i].style.borderColor = "black";
+      fields[i].style.borderColor = 'black';
     }
   }
 
   if (!valid) {
-    errorMsg.classList.toggle("hidden");
-    return false
-  } else {
-    const book = new Book(name.value, author.value, pages.value, read);
-    showForm()
-    return book
+    errorMsg.classList.toggle('hidden');
+    return false;
   }
+  const book = new Book(name.value, author.value, pages.value, read);
+  showForm();
+  return book;
 }
 
 function addBookToLibrary() {
@@ -108,8 +112,6 @@ function addBookToLibrary() {
   render();
 }
 
-function clearDOM() {
-  document.querySelector(".row").innerHTML = "";
-}
+document.getElementById('save').addEventListener('click', addBookToLibrary);
 
-window.addEventListener(onload, render())
+window.addEventListener('onload', render());
